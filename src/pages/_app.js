6de1f9777e69acf6383db3056b18bcb22cc0437c
Alex Barashkov <lnikell@gmail.com>
2021-01-14 13:29:18 +0100

    refactor: change folder structure and update deps

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css';
+
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-filename-extension
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
