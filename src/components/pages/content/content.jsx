0dc73d10f5757186c4288d6cf9114b98f50cc15c
Thi Van Le <vannnyle@gmail.com>
2022-02-22 18:24:27 +0200

    chore: swap deps in package.json, remove content component

--- src/components/pages/content/content.jsx
+++
@@ -1,5 +0,0 @@
-const Content = () => (
-  <div className="mx-auto max-w-5xl my-10 text-center">Pixel Point Next.js Tailwind Starter</div>
-);
-
-export default Content;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,8 @@
-import Content from 'components/pages/content/content';
 import LayoutMain from 'layouts/layouts/layout-main';
 
 const Home = () => (
   <LayoutMain>
-    <Content />
+    <div className="mx-auto my-10 max-w-5xl text-center">Pixel Point Next.js Tailwind Starter</div>
   </LayoutMain>
 );
 
