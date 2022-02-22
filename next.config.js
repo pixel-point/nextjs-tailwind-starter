ffabde48aba13085b64149f4de6af360f0633260
Thi Van Le <vannnyle@gmail.com>
2022-02-22 16:10:18 +0200

    chore: update depencencies, add utilities css

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ module.exports = {
                   },
                 },
                 'prefixIds',
+                'removeDimensions',
               ],
             },
           },
--- src/components/pages/content/content.jsx
+++ src/components/pages/content/content.jsx
@@ -1,3 +1,5 @@
-const Content = () => <div>Pixel Point Next.js Tailwind Starter</div>
+const Content = () => (
+  <div className="mx-auto max-w-5xl my-10 text-center">Pixel Point Next.js Tailwind Starter</div>
+);
 
 export default Content;
