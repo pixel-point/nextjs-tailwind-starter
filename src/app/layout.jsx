11a602502cab10cbdd338f395f5c8bfdd665912a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-12-13 20:22:47 +0100

    feat: add basic examples

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,16 @@
+import Footer from 'components/shared/footer';
+import Header from 'components/shared/header';
+
+import 'styles/global.css';
+
 // eslint-disable-next-line react/prop-types
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <head />
-    <body>{children}</body>
+    <body>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </body>
   </html>
 );
 
