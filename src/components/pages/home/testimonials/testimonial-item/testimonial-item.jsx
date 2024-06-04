fb229d0a16168d9d52769711ab4ecbccdc127a0a
VBasko <vvbasko@outlook.com>
2024-06-04 13:07:16 -0300

    feat: testimonials section

---
+++ src/components/pages/home/testimonials/testimonial-item/testimonial-item.jsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+
+import IconBlured from 'components/shared/icon-blured';
+
+const TestimonialItem = ({ author, text, icon }) => (
+  <div className="grow max-w-[522px] flex flex-col">
+    {icon && (
+      <IconBlured size="large" src={icon} alt="Testimonial decorative icon" className="mb-5" />
+    )}
+
+    <p
+      className="text-grey-200 text-2xl leading-normal -tracking-[0.01em] mb-8"
+      dangerouslySetInnerHTML={{ __html: text }}
+    />
+
+    <div className="flex items-start space-x-3">
+      <div className="flex items-center justify-center rounded-full size-[44px] leading-none overflow-hidden">
+        <Image src={author.photo} alt={`${author.name} photo`} size={44} />
+      </div>
+
+      <div className="flex flex-col space-y-[2px]">
+        <p className="text-lg text-white leading-tight">{author.name}</p>
+        <p className="text-grey-200 text-[15px] leading-tight">{author.position}</p>
+      </div>
+    </div>
+  </div>
+);
+
+TestimonialItem.propTypes = {
+  author: {
+    name: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+  },
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+export default TestimonialItem;
--- src/components/pages/home/testimonials/testimonials.jsx
+++ src/components/pages/home/testimonials/testimonials.jsx
@@ -1,3 +1,50 @@
-const Testimonials = () => <section>Testimonials section</section>;
+import Image from 'next/image';
+
+import firstTestimonialIcon from './images/testimonial-1.svg';
+import secondTestimonialIcon from './images/testimonial-2.svg';
+import firstTestimonialAuthorPhoto from './images/testimonial-avatar-1.png';
+import secondTestimonialAuthorPhoto from './images/testimonial-avatar-2.png';
+import testimonialsBgImage from './images/testimonials-bg.png';
+import TestimonialItem from './testimonial-item';
+
+const testimonials = [
+  {
+    author: {
+      name: 'John Smith',
+      position: 'IT Director at Reflex',
+      photo: firstTestimonialAuthorPhoto,
+    },
+    text: 'Thanks to DevOptima, our team can focus more on <span class="text-grey-100 font-medium">innovation</span> and less on operational challenges. Their <span class="text-grey-100 font-medium">cloud management solutions</span> are top-notch.',
+    icon: firstTestimonialIcon,
+  },
+  {
+    author: {
+      name: 'Jane Doe',
+      position: 'Tech Lead at Dribble',
+      photo: secondTestimonialAuthorPhoto,
+    },
+    text: 'DevOptima has been a game-changer for us. With their support, we&apos;ve shifted our focus <span class="text-grey-100 font-medium">from operational hurdles to pushing boundaries in innovation</span>.',
+    icon: secondTestimonialIcon,
+  },
+];
+
+const Testimonials = () => (
+  <section id="testimonials" className="h-[850px] relative">
+    <div className="absolute w-full h-full pointer-events-none -z-[1]">
+      <Image src={testimonialsBgImage} className="max-w-none" fill />
+    </div>
+
+    <div className="container h-full flex gap-x-[88px] items-center justify-center">
+      {testimonials.map((item, index) => (
+        <TestimonialItem
+          key={`testimonial-item-${index}`}
+          author={item.author}
+          text={item.text}
+          icon={item.icon}
+        />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
