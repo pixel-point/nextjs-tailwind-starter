4a7111bba09483ee25ef523735802ad19c53f2d9
VBasko <vvbasko@outlook.com>
2024-06-04 13:02:41 -0300

    refactor: features component

--- src/components/pages/home/about/features/features.jsx
+++ src/components/pages/home/about/features/features.jsx
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import React from 'react';
+import IconBlured from 'components/shared/icon-blured';
 
 import AutMastery from './images/automation-mastery.svg';
 import CiCdIcon from './images/ci-cd-implementation.svg';
@@ -34,10 +33,7 @@ const Features = () => (
     {features.map((item, index) => (
       <li key={`feature-item-${index}`} className="max-w-[242px] min-h-[127px]">
         <div className="flex flex-col space-y-[14px]">
-          <div className="relative self-start">
-            <Image src={item.icon} width={36} height={36} alt={`${item.title} icon`} />
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 size-[22px] bg-blue-200 blur-[40px] -z-[1]" />
-          </div>
+          <IconBlured className="self-start" src={item.icon} alt={item.title} />
 
           <div className="flex flex-col space-y-2">
             <h3>{item.title}</h3>
